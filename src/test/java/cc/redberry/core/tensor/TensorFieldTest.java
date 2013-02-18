5de8707ee1f4b3b0141bfa1426e65b8eb63fa9cf
Dmitriy Bolotin <bolotin.dmitriy@gmail.com>
2013-02-19 03:01:02 +0400

    Failed test added.

--- src/test/java/cc/redberry/core/tensor/TensorFieldTest.java
+++ src/test/java/cc/redberry/core/tensor/TensorFieldTest.java
@@ -25,18 +25,25 @@ package cc.redberry.core.tensor;
 import junit.framework.Assert;
 import org.junit.Test;
 
+import static cc.redberry.core.tensor.Tensors.parse;
+
 /**
  * @author Dmitry Bolotin
  * @author Stanislav Poslavsky
  */
 public class TensorFieldTest {
-
     @Test
     public void testIterator() {
-        Tensor t = Tensors.parse("f[a,b,c]");
+        Tensor t = parse("f[a,b,c]");
         int i = 0;
         for (Tensor c : t)
             ++i;
         Assert.assertEquals(i, 3);
+    }
+
+    @Test
+    public void testNames() throws Exception {
+        Tensor t1 = parse("F[S_A'^B']"), t2 = parse("F[S^A'_B']");
+        Assert.assertEquals(((TensorField) t1).getName(), ((TensorField) t2).getName());
     }
 }
